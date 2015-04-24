3a96779d2d93175d439b4060eb59611f11dc92bf
zhuoliuzhou <zhuoliuzhou@gmail.com>
2015-04-24 15:34:43 +0800

    Solution to Assignment2
    I have writtrn code based on the example given that solves for cached
    mean. The new code has the same logic. First it checks whether there are
    already inverse cached. If yes, then it check whether the matrix we type
    in is the same as the cached matrix. If again yes, then it return the
    cached "m", inverse of the matrix. If no to either one, it means we want
    to calculate inverse of a new matrix. The function will then cached the
    matrix and its inverse.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  if(!exists("m"))  m <- NULL #First check whether there are already cached variable
+                              #if not, then create one with null
+  set <- function(y){         #this saves the new matrix
+    h <<- y
+    m <<- NULL
+      
+  }
+  get <- function() h
+  setinv <- function(inv) assign("m",inv,envir = .GlobalEnv) #this caches the inverse in the global environment
+  getinv <- function() m
+  list(set=set,get=get,setinv = setinv,getinv=getinv)
 }
 
 
@@ -12,4 +22,19 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- makeCacheMatrix(x)$getinv()
+  if(!is.null(m)){
+    if(all(makeCacheMatrix()$get()==x)){ #this checks whether the matrix we want to calculate equals the one cached already
+      message("getting cached data")
+      return(m)
+    }
+  }
+  makeCacheMatrix()$set(x)
+  data <- x
+  m <- solve(data,...)
+  makeCacheMatrix(x)$setinv(m)
+  m
 }
+
+
+
